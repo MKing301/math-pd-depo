2d28a373e8c5150aaa7a9b42d47292519b689cc9
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-06-21 20:11:27 -0400

    Add html title in views for rendered templates

--- pd_app/main/views.py
+++ pd_app/main/views.py
@@ -7,9 +7,9 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
 
 
 @main.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
