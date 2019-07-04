983cbe63ef7cca16a500df3d2d931b08a84a9eaa
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-07-04 10:57:37 -0400

    Add register route to user's views

--- pd_app/users/views.py
+++ pd_app/users/views.py
@@ -2,13 +2,21 @@ from datetime import datetime
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 from pd_app.models import User
-from pd_app.users.forms import ContactForm
+from pd_app.users.forms import RegistrationForm, ContactForm
 
 
 
 users = Blueprint('users', __name__)
 
 
+@users.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        redirect(url_for('main.home'))
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+    
+    
 @users.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
@@ -16,6 +24,6 @@ def contact():
         user = User(request.form['first_name'],
                     request.form['last_name'], request.form['email'],
                     request.form['user_request'], datetime.utcnow())
-        return redirect(url_for('contact'))
+        return redirect(url_for('users.contact'))
     else:
         return render_template('contact.html', Title="Contact", form=form)
