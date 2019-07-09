40b051c81824acca1f17a845ed05375911c1f4cc
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-07-09 16:55:59 -0400

    Add contact table to database setup

--- pd_app/database_setup.py
+++ pd_app/database_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    '''A user of the catalog app program. Users will have
+    '''A registered user of the program. Users will have
        the following attributes:
        Attibute(s):
        id - unique id number for each user
@@ -28,6 +28,27 @@ class User(Base):
     email = Column(String, nullable=False)
     password = Column(String, nullable=False)
     inserted = Column(DateTime, nullable=False)
+
+
+class Contact(Base):
+    '''A contact submitting a request or feedback about the program. 
+       This person will have the following attributes:
+       Attibute(s):
+       id - unique id number for each user
+       first_name - user's first name
+       last_name - user's last name
+       email - user's email address
+       user_request - user's request
+       created_date - date user's request submitted
+    '''
+    __tablename__ = 'contact'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    user_request = Column(Text, nullable=False)
+    created_date = Column(DateTime, nullable=False)
 
 
 engine = create_engine(os.environ.get('LOCAL_DB_URI_MATH'))
