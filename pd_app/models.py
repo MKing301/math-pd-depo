8b85dc0f1f184a49e4c8335b1312d3997ef0b353
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-07-10 17:52:06 -0400

    Add models

---
+++ pd_app/models.py
@@ -0,0 +1,26 @@
+from flask import current_app
+from pd_app import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(254), nullable=False)
+    inserted = db.Column(db.DateTime, nullable=False)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    user_request = db.Column(db.Text, nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False)
