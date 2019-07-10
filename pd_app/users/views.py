01516a3aea17bac325d458e0bba615e6a1b260d8
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-07-10 17:39:58 -0400

    Add db update to register route in user views

--- pd_app/users/views.py
+++ pd_app/users/views.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
-from pd_app.models import User
-from pd_app.users.forms import RegistrationForm, LoginForm, ContactForm
+from pd_app import db, bcrypt
+from pd_app.models import User, Contact
+from pd_app.users.forms import RegistrationForm, LoginForm, SearchForm, ContactForm
+import pytz
 
 
 
@@ -12,8 +14,20 @@ users = Blueprint('users', __name__)
 @users.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        redirect(url_for('main.home'))
+        return redirect(url_for('main.home'))
     form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user_register = User(first_name=form.first_name.data,
+                             last_name=form.last_name.data,
+                             username=form.username.data,
+                             email=form.email.data,
+                             password=hashed_password,
+                             inserted=datetime.now(pytz.timezone('US/Eastern')))
+        db.session.add(user_register)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to log in.', 'success')
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
 
