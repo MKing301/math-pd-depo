e64fc8d6c423ed25824a4bd288286ee48e88f3fe
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-07-10 17:38:31 -0400

    Add database environment variables to config file

--- pd_app/config.py
+++ pd_app/config.py
@@ -3,3 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY_LOCAL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('LOCAL_DB_URI_MATH')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
