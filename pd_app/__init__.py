b33c24adabb71c53510b26e736eae8bfe2b69ae6
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-07-11 17:59:34 -0400

    Add mail app to init

--- pd_app/__init__.py
+++ pd_app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 from pd_app.config import Config
 
 
@@ -16,7 +17,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-
+mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -25,6 +26,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
 
     from pd_app.main.views import main
