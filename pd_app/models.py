69e2f690f4068e3e05b53de817165981e89ded10
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-07-12 17:24:41 -0400

    Add additional imports to models

--- pd_app/models.py
+++ pd_app/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from pd_app import db, login_manager
 from flask_login import UserMixin
@@ -15,6 +17,20 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(254), nullable=False)
     inserted = db.Column(db.DateTime, nullable=False)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id':self.id}).decode('utf-8')
+
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
 
 class Contact(db.Model):
